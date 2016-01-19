67eb9f01dcb71c9f843e12466426dcab8a64ba05
Gabe Medrash <gmedrash@gmail.com>
2016-01-19 12:38:19 -0800

    add package to hold mocks to use consistently throughout all testing, where feasible

---
+++ packages/beaut-test-utils/README.md
@@ -0,0 +1,4 @@
+beaut-test-utils
+===============================
+
+A repo dedicated to holding mocks used consistently throughout beaut component testing.
---
+++ packages/beaut-test-utils/mocks/colors.js
@@ -0,0 +1,17 @@
+/**
+ * 11 step diverging color scale
+ * take from colorbrewer (http://colorbrewer2.org/)
+ */
+export default [
+  '#a50026', // dark red
+  '#d73027',
+  '#f46d43',
+  '#fdae61',
+  '#fee090',
+  '#ffffbf', // light yellow
+  '#e0f3f8',
+  '#abd9e9',
+  '#74add1',
+  '#4575b4',
+  '#313695' // dark blue
+];
