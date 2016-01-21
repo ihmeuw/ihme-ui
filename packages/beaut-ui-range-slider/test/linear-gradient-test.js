44ebbb488a0703462538819c33ef04bfd4c85e97
Gabe Medrash <gmedrash@gmail.com>
2016-01-20 16:39:12 -0800

    finish <LinearGradient /> tests

--- packages/beaut-ui-range-slider/test/linear-gradient-test.js
+++ packages/beaut-ui-range-slider/test/linear-gradient-test.js
@@ -6,20 +6,41 @@ import { colorSteps } from '@ihme/beaut-test-utils';
 import LinearGradient from '../src/components/linear-gradient';
 
 describe('<LinearGradient />', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<LinearGradient colors={colorSteps} />);
+  it('renders a linearGradient node as HTML text', () => {
+    const wrapper = shallow(<LinearGradient colors={colorSteps} />);
+    const renderedHTML = wrapper.find('linearGradient').render();
+    expect(renderedHTML.find('#choropleth-linear-gradient-def')).to.have.length(1);
   });
 
   it('renders as many SVG stops as color steps passed through props', () => {
+    const wrapper = shallow(<LinearGradient colors={colorSteps} />);
+
     expect(wrapper.children()).to.have.length(colorSteps.length);
   });
 
   it('renders each SVG stop with a key, offset and stopColor prop', () => {
+    const wrapper = shallow(<LinearGradient colors={colorSteps} />);
+
     wrapper.children().forEach(node => {
       expect(node.type()).to.equal('stop');
       expect(node.props()).to.include.keys('offset', 'stopColor');
     });
+  });
+
+  it('defines x-axis coord start and end of the vector gradient', () => {
+    const wrapper = shallow(
+      <LinearGradient
+        colors={colorSteps}
+        x1={25}
+        x2={75}
+      />
+    );
+    const node = wrapper.find('linearGradient');
+
+    expect(node.prop('x1')).to.be.a('string')
+      .and.to.equal('25%');
+
+    expect(node.prop('x2')).to.be.a('string')
+      .and.to.equal('75%');
   });
 });
