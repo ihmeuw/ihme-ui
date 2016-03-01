606adf334833be6853777467d6112c7f38095bf6
Brett Vitaz <vitazbt@uw.edu>
2016-03-01 15:36:12 -0800

    add demo for Line component.

---
+++ src/ui/line/demo/app.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import maxBy from 'lodash/maxby';
+import minBy from 'lodash/minby';
+import map from 'lodash/map';
+import uniqBy from 'lodash/uniqBy';
+
+import { dataGenerator } from '../../../test-utils';
+import { LineChart, MultiLine } from '../src';
+
+
+const keyField = 'year_id';
+const valueField = 'value';
+
+const usaData = dataGenerator({
+  keyField,
+  valueField,
+  length: 10,
+  dataQuality: 'best',
+  useDates: true
+});
+
+const canadaData = dataGenerator({
+  keyField,
+  valueField,
+  length: 10,
+  dataQuality: 'best',
+  useDates: true
+});
+
+const lineData = [{ location: 'USA', values: usaData }, { location: 'Canada', values: canadaData }];
+
+const data = [...usaData, ...canadaData];
+const yDomain = [minBy(data, valueField)[valueField], maxBy(data, valueField)[valueField]];
+const xDomain = map(uniqBy(data, keyField), (obj) => { return (obj[keyField]); });
+
+class App extends React.Component {
+  render() {
+    return (
+      <LineChart
+        xDomain={xDomain}
+        xScaleType="point"
+        yDomain={yDomain}
+        yScaleType="linear"
+        width={800}
+        height={600}
+      >
+        <MultiLine
+          data={lineData}
+          keyField={'location'}
+          dataField={'values'}
+          dataAccessors={{ x: keyField, y: valueField }}
+        />
+      </LineChart>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
