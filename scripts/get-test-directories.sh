9075db7ca2324f498ed95f42717992fbbd63e6ce
Gabe Medrash <gmedrash@gmail.com>
2016-03-09 16:46:45 -0800

    use find utility instead of a bunch of nested bash logic to find all test directories

--- scripts/get-test-directories.sh
+++ scripts/get-test-directories.sh
@@ -3,25 +3,8 @@ set -e
 
 TEST_DIRS=""
 
-for f in src/*; do
-  if [ -n "$TEST_ONLY" ] && [ `basename $f` != "$TEST_ONLY" ]; then
-    continue
-  fi
-
-  if [ -d "$f/test" ]; then
-    TEST_DIRS="$f/test $TEST_DIRS"
-  fi
-
-  # nested test dirs
-  for j in $f/*; do
-      if [ -n "$TEST_ONLY" ] && [ `basename $j` != "$TEST_ONLY" ]; then
-        continue
-      fi
-
-      if [ -d "$j/test" ]; then
-        TEST_DIRS="$j/test $TEST_DIRS"
-      fi
-  done
+for dir in $(find ./src -d -name 'test' -type d); do
+    TEST_DIRS="$dir $TEST_DIRS"
 done
 
 echo $TEST_DIRS
