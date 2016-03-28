b9e3601a888359ae90023915755059a3950000ff
Gabe Medrash <gmedrash@gmail.com>
2016-03-28 12:42:37 -0700

    reorganize; use css modules for default styling

--- src/ui/axis/index.js
+++ src/ui/axis/index.js
@@ -1,3 +1,3 @@
-export { default as default } from './src/components/axis';
-export { default as XAxis } from './src/components/xaxis';
-export { default as YAxis } from './src/components/yaxis';
+export { default as Axis } from './src/axis';
+export { default as XAxis } from './src/x-axis';
+export { default as YAxis } from './src/y-axis';
---
+++ src/ui/axis/src/x-axis.jsx
@@ -0,0 +1,47 @@
+import React, { PropTypes } from 'react';
+import { assign, omit } from 'lodash';
+
+import Axis, { sharedPropTypes, calcTranslate } from './axis';
+
+const propTypes = assign({}, sharedPropTypes, {
+  /* OVERRIDE - where to position ticks relative to axis line */
+  position: PropTypes.oneOf(['top', 'bottom']),
+
+  /* scales are provided by axis-chart, only x-scale is used by XAxis */
+  scales: PropTypes.shape({
+    x: PropTypes.func.isRequired,
+    y: PropTypes.func
+  }),
+
+  /*
+    dimensions are provided by axis-chart
+    used for calculating translate, required if translate is not specified
+  */
+  dimensions: PropTypes.shape({
+    width: PropTypes.number,
+    height: PropTypes.number
+  })
+});
+
+const defaultProps = {
+  position: 'bottom'
+};
+
+const XAxis = (props) => {
+  const { scales, dimensions, position, translate } = props;
+  const childProps = omit(props, ['scales', 'translate', 'dimensions']);
+  const translation = translate || calcTranslate(position, dimensions);
+
+  return (
+    <Axis
+      scale={scales.x}
+      translate={translation}
+      {...childProps}
+    />
+  );
+};
+
+XAxis.propTypes = propTypes;
+XAxis.defaultProps = defaultProps;
+
+export default XAxis;
