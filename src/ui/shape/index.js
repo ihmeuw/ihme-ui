cd6ea109f1bbd94f55dfc75923071b3f4165d00e
Gabe Medrash <gmedrash@gmail.com>
2016-03-29 18:15:58 -0700

    flatten src dirs

--- src/ui/shape/index.js
+++ src/ui/shape/index.js
@@ -1,5 +1,5 @@
-export { default as Area } from './src/components/area';
-export { default as Line } from './src/components/line';
-export { default as MultiLine } from './src/components/multi-line';
-export { default as ScatterPlot } from './src/components/scatter-plot';
-export { default as Symbol, SYMBOL_TYPES } from './src/components/symbol';
+export { default as Area } from './src/area';
+export { default as Line } from './src/line';
+export { default as MultiLine } from './src/multi-line';
+export { default as ScatterPlot } from './src/scatter-plot';
+export { default as Symbol, SYMBOL_TYPES } from './src/symbol';
