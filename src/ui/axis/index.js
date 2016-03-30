6d61f13c00c9074a16b8e140cb6ae7e7400a93f3
Gabe Medrash <gmedrash@gmail.com>
2016-03-29 17:59:24 -0700

    expose base axis component as default export

--- src/ui/axis/index.js
+++ src/ui/axis/index.js
@@ -1,3 +1,3 @@
-export { default as Axis } from './src/axis';
+export { default as default } from './src/axis';
 export { default as XAxis } from './src/x-axis';
 export { default as YAxis } from './src/y-axis';
