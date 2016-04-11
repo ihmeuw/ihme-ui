059874f01f2ffae0cc3c0d8e110b6e4fd09d9d80
Gabe Medrash <gmedrash@gmail.com>
2016-04-10 22:46:10 -0700

    extract css modules into stylesheets for distribution; configure autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
 'use strict';
 
+var path = require('path');
 var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   output: {
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
+    path: __dirname
   },
   module: {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
-      },
+        loaders: [
+          ExtractTextPlugin.extract('style-loader'),
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]',
+          'postcss-loader'
+        ]
+      }
     ]
   },
-  postcss: [autoprefixer],
+  plugins: [
+    new ExtractTextPlugin('style/' + path.parse(process.argv[2]).name + '.css')
+  ],
+  postcss: [autoprefixer({ browsers: ['last 2 versions'] })],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
