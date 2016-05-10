feaca22d5107f7017a0c219124168847a2c4258c
Aaron Keel <akeel@uw.edu>
2016-05-09 22:00:40 -0700

    Updated tests for updated data.js

--- src/test-utils/test/data.test.js
+++ src/test-utils/test/data.test.js
@@ -1,95 +1,116 @@
-import { expect } from 'chai';
-import { isNull } from 'lodash';
+const expect = require('chai').expect;
+const dataGenerator = require('./data');
 
-import { dataGenerator } from '../data';
+describe('data generator', () => {
+  it('returns an array', () => {
+    expect(dataGenerator()).to.be.an('array');
+  });
 
-describe('data', () => {
   it('generates an array of 200 objects', () => {
-    expect(dataGenerator()).to.be.an('array')
+    const config = {
+      primaryKeys: [{ name: 'foo', values: ['bar'] }],
+      valueKeys: [{ name: 'lamp', range: [1, 200] }],
+      length: 200
+    };
+
+    expect(dataGenerator(config)).to.be.an('array')
       .with.length(200)
       .and.to.have.deep.property('[0]')
       .that.is.an('object')
-      .with.keys('location_id', 'value', 'ub', 'lb');
+      .with.keys('foo', 'lamp', 'year', 'id');
+  });
+
+  it('generates an array with values in a certain range', () => {
+    const config = {
+      primaryKeys: [{ name: 'foo', values: ['bar'] }],
+      valueKeys: [{ name: 'lamp', range: [1, 200] }],
+      length: 200
+    };
+
+    const data = dataGenerator(config);
+    for (let i = 0; i < data.length; i++) {
+      expect(data[i].lamp).to.be.within(1, 200);
+    }
+  });
+
+  it('generates an array with objects such that lb < mean < ub', () => {
+    const config = {
+      primaryKeys: [{ name: 'foo', values: ['bar'] }],
+      valueKeys: [
+        { name: 'mean', range: [100, 200] },
+        { name: 'ub', range: [110, 210] },
+        { name: 'lb', range: [90, 190] }
+      ],
+      length: 200
+    };
+
+    const data = dataGenerator(config);
+    for (let i = 0; i < data.length; i++) {
+      expect(data[i].mean).to.be.within(data[i].lb, data[i].ub);
+    }
+  });
+
+  it('generates data with increasing mean values', () => {
+    const config = {
+      primaryKeys: [{ name: 'foo', values: ['bar'] }],
+      valueKeys: [
+        { name: 'mean', range: [100, 200] }
+      ],
+      length: 200
+    };
+
+    const data = dataGenerator(config);
+    for (let i = 0; i < data.length - 1; i++) {
+      expect(data[i + 1].mean).to.be.above(data[i].mean);
+    }
+  });
+
+  it('generates data with decreasing mean values', () => {
+    const config = {
+      primaryKeys: [{ name: 'foo', values: ['bar'] }],
+      valueKeys: [
+        { name: 'mean', range: [300, 100] }
+      ],
+      length: 200
+    };
+
+    const data = dataGenerator(config);
+    for (let i = 0; i < data.length - 1; i++) {
+      expect(data[i + 1].mean).to.be.below(data[i].mean);
+    }
+  });
+
+  it('generates unique data points', () => {
+    const config = {
+      primaryKeys: [
+        { name: 'AAA', values: [1, 2, 3] },
+        { name: 'BBB', values: [1, 2] },
+        { name: 'CCC', values: [1, 2] }
+      ],
+      length: 2
+    };
+
+    const data = dataGenerator(config);
+    for (let i = 0; i < data.length - 1; i++) {
+      for (let j = i + 1; j < data.length; j++) {
+        expect([data[i].AAA, data[i].BBB, data[i].CCC, data[i].year])
+        .to.not.eql([data[j].AAA, data[j].BBB, data[j].CCC, data[j].year]);
+      }
+    }
   });
 
   it('passes custom props to each datum', () => {
-    expect(dataGenerator({ unicorns: 'rainbows' }))
+    const config = {
+      primaryKeys: [
+        { name: 'AAA', values: [1, 2, 3] },
+        { name: 'BBB', values: [1, 2] },
+        { name: 'unicorns', values: ['rainbows'] }
+      ],
+      length: 2
+    };
+    expect(dataGenerator(config))
       .to.have.deep.property('[0]')
       .that.is.an('object')
       .with.property('unicorns', 'rainbows');
-  });
-
-  it('creates data that has null values', () => {
-    const data = dataGenerator({ dataQuality: 'worst' });
-    const hasNullValues = data.some((datum) => {
-      return isNull(datum.value);
-    });
-
-    expect(hasNullValues).to.be.true;
-  });
-
-  it('generates dates for keyfield', () => {
-    const length = 20;
-    const startYear = 2016;
-    const data = dataGenerator({
-      keyField: 'year_id',
-      useDates: true,
-      length,
-      startYear
-    });
-
-    expect(data)
-      .to.have.length(20);
-
-    expect(data)
-      .to.have.deep.property('[0].year_id')
-      .that.is.a('number')
-      .that.equals(startYear - length + 1);
-
-    expect(data)
-      .to.have.deep.property('[19].year_id')
-      .that.is.a('number')
-      .that.equals(startYear);
-  });
-
-  it('creates ub > value > lb', () => {
-    const data = dataGenerator({
-      length: 20
-    });
-    data.forEach((datum) => {
-      expect(datum.ub).to.be.above(datum.value);
-      expect(datum.value).to.be.above(datum.lb);
-    });
-  });
-
-  it('creates increasing data', () => {
-    const data = dataGenerator({
-      length: 20
-    });
-    expect(data[0].value).to.be.below(data[19].value);
-  });
-
-  it('creates decreasing data', () => {
-    const data = dataGenerator({
-      dataTrend: 'decreasing',
-      length: 20
-    });
-    expect(data[0].value).to.be.above(data[19].value);
-  });
-
-  it('creates exponential growth data(increases)', () => {
-    const data = dataGenerator({
-      dataTrend: 'exponentialGrowth',
-      length: 20
-    });
-    expect(data[0].value).to.be.below(data[19].value);
-  });
-
-  it('creates exponential decay data(decreases)', () => {
-    const data = dataGenerator({
-      dataTrend: 'exponentialDecay',
-      length: 20
-    });
-    expect(data[0].value).to.be.above(data[19].value);
   });
 });
