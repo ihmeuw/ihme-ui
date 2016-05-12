2cea2de843f2f8f921e974c880231b0c9e7e742e
Aaron Keel <akeel@ihmeadmins-MacBook-Pro.local>
2016-05-12 16:49:50 -0700

    dumb with git

--- src/test-utils/test/data.test.js
+++ src/test-utils/test/data.test.js
@@ -17,7 +17,6 @@ describe('data generator', () => {
       .with.length(200)
       .and.to.have.deep.property('[0]')
       .that.is.an('object')
-<<<<<<< HEAD
       .with.keys('foo', 'lamp', 'year_id', 'id');
   });
 
@@ -28,46 +27,23 @@ describe('data generator', () => {
       length: 200
     };
 
-=======
-      .with.keys('foo', 'lamp', 'year', 'id');
-  });
-
-  it('generates an array with values in a certain range', () => {
-    const config = {
-      primaryKeys: [{ name: 'foo', values: ['bar'] }],
-      valueKeys: [{ name: 'lamp', range: [1, 200] }],
-      length: 200
-    };
-
->>>>>>> c72f4bd94ba2357c0e050cccbb0ac09355715573
     const data = dataGenerator(config);
     for (let i = 0; i < data.length; i++) {
       expect(data[i].lamp).to.be.within(1, 200);
     }
   });
 
-<<<<<<< HEAD
   it('generates an array with objects such that mean_lb < mean < mean_ub', () => {
     const config = {
       primaryKeys: [{ name: 'foo', values: ['bar'] }],
       valueKeys: [
         { name: 'mean', range: [100, 200], uncertainty: true }
-=======
-  it('generates an array with objects such that lb < mean < ub', () => {
-    const config = {
-      primaryKeys: [{ name: 'foo', values: ['bar'] }],
-      valueKeys: [
-        { name: 'mean', range: [100, 200] },
-        { name: 'ub', range: [110, 210] },
-        { name: 'lb', range: [90, 190] }
->>>>>>> c72f4bd94ba2357c0e050cccbb0ac09355715573
       ],
       length: 200
     };
 
     const data = dataGenerator(config);
     for (let i = 0; i < data.length; i++) {
-<<<<<<< HEAD
       expect(data[i].mean).to.be.within(data[i].mean_lb, data[i].mean_ub);
     }
   });
@@ -91,61 +67,6 @@ describe('data generator', () => {
     }
   });
 
-=======
-      expect(data[i].mean).to.be.within(data[i].lb, data[i].ub);
-    }
-  });
-
-  it('generates data with increasing mean values', () => {
-    const config = {
-      primaryKeys: [{ name: 'foo', values: ['bar'] }],
-      valueKeys: [
-        { name: 'mean', range: [100, 200] }
-      ],
-      length: 200
-    };
-
-    const data = dataGenerator(config);
-    for (let i = 0; i < data.length - 1; i++) {
-      expect(data[i + 1].mean).to.be.above(data[i].mean);
-    }
-  });
-
-  it('generates data with decreasing mean values', () => {
-    const config = {
-      primaryKeys: [{ name: 'foo', values: ['bar'] }],
-      valueKeys: [
-        { name: 'mean', range: [300, 100] }
-      ],
-      length: 200
-    };
-
-    const data = dataGenerator(config);
-    for (let i = 0; i < data.length - 1; i++) {
-      expect(data[i + 1].mean).to.be.below(data[i].mean);
-    }
-  });
-
-  it('generates unique data points', () => {
-    const config = {
-      primaryKeys: [
-        { name: 'AAA', values: [1, 2, 3] },
-        { name: 'BBB', values: [1, 2] },
-        { name: 'CCC', values: [1, 2] }
-      ],
-      length: 2
-    };
-
-    const data = dataGenerator(config);
-    for (let i = 0; i < data.length - 1; i++) {
-      for (let j = i + 1; j < data.length; j++) {
-        expect([data[i].AAA, data[i].BBB, data[i].CCC, data[i].year])
-        .to.not.eql([data[j].AAA, data[j].BBB, data[j].CCC, data[j].year]);
-      }
-    }
-  });
-
->>>>>>> c72f4bd94ba2357c0e050cccbb0ac09355715573
   it('passes custom props to each datum', () => {
     const config = {
       primaryKeys: [
@@ -155,17 +76,10 @@ describe('data generator', () => {
       ],
       length: 2
     };
-<<<<<<< HEAD
     const data = dataGenerator(config);
     for (let i = 0; i < data.length; i++) {
       expect(data[i]).to.be.an('object')
       .with.property('unicorns', 'rainbows');
     }
-=======
-    expect(dataGenerator(config))
-      .to.have.deep.property('[0]')
-      .that.is.an('object')
-      .with.property('unicorns', 'rainbows');
->>>>>>> c72f4bd94ba2357c0e050cccbb0ac09355715573
   });
 });
