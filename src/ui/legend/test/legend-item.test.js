09f5703bec844eed60b0a16747ea3b35d9a9a06a
Gabe Medrash <gmedrash@gmail.com>
2016-05-21 15:13:54 -0700

    finish LegendItem tests

--- src/ui/legend/test/legend-item.test.js
+++ src/ui/legend/test/legend-item.test.js
@@ -13,10 +13,9 @@ import { Symbol } from '../../shape';
 describe('<LegendItem />', () => {
   const item = {
     estimateStage: 'GBD final',
-    symbol: {
-      color: 'red',
-      type: 'circle'
-    }
+    symbolColor: 'red',
+    symbolType: 'triangle',
+    arbitraryField: 456
   };
 
   it('accepts a labelKey that is a string', () => {
@@ -36,34 +35,48 @@ describe('<LegendItem />', () => {
   });
 
   it('accepts a symbolTypeKey that is a string', () => {
-    const wrapper = shallow(<LegendItem item={item} symbolTypeKey="symbol.type" />);
-    expect(wrapper).to.contain(<Symbol type="circle" />);
+    const wrapper = shallow(<LegendItem item={item} symbolTypeKey="symbolType" />);
+    expect(wrapper).to.contain(<Symbol type="triangle" />);
   });
 
   it('accepts a symbolTypeKey that is a function that is called with the item', () => {
     const spy = sinon.spy((itemObj) => {
-      return itemObj.symbol.type;
+      return itemObj.symbolType;
     });
     const wrapper = shallow(<LegendItem item={item} symbolTypeKey={spy} />);
 
-    expect(wrapper).to.contain(<Symbol type="circle" />);
+    expect(wrapper).to.contain(<Symbol type="triangle" />);
     expect(spy.called).to.be.true;
     expect(spy.calledWith(item)).to.be.true;
   });
 
   it('accepts a symbolColorKey that is a string', () => {
-    const wrapper = shallow(<LegendItem item={item} symbolColorKey="symbol.color" />);
+    const wrapper = shallow(<LegendItem item={item} symbolColorKey="symbolColor" />);
     expect(wrapper).to.contain(<Symbol color="red" />);
   });
 
   it('accepts a symbolColorKey that is a function that is called with the item', () => {
     const spy = sinon.spy((itemObj) => {
-      return itemObj.symbol.color;
+      return itemObj.symbolColor;
     });
     const wrapper = shallow(<LegendItem item={item} symbolColorKey={spy} />);
 
     expect(wrapper).to.contain(<Symbol color="red" />);
     expect(spy.called).to.be.true;
     expect(spy.calledWith(item)).to.be.true;
+  });
+
+  it('renders a custom label component', () => {
+    /* eslint-disable react/prop-types */
+    // a custom label component is called with the item object
+    function CustomComponent(props) {
+      return <tspan>{props.item.arbitraryField}</tspan>;
+    }
+    /* eslint-enable react/prop-types */
+
+    const wrapper = shallow(<LegendItem item={item} labelRenderer={CustomComponent} />);
+    const textNode = wrapper.find('text');
+    expect(textNode).to.have.descendants('tspan');
+    expect(textNode.find('tspan')).to.have.text(String(item.arbitraryField));
   });
 });
