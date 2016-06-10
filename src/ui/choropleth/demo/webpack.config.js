7c259920f4aa6959b5fdb8fb229d4a17e89f74e5
Brett Vitaz <vitazbt@uw.edu>
2016-06-09 18:09:51 -0700

    add webpack notifier to choropleth demo

--- src/ui/choropleth/demo/webpack.config.js
+++ src/ui/choropleth/demo/webpack.config.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var autoprefixer = require('autoprefixer');
+var WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
   devtool: 'source-map',
@@ -11,6 +12,9 @@ module.exports = {
     path: __dirname,
     filename: 'bundle.js'
   },
+  plugins: [
+    new WebpackNotifierPlugin({alwaysNotify: true})
+  ],
   module: {
     loaders: [
       {
