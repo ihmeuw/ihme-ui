95d3f3ac864fd13a94868458cc8d49e92041a0a0
Brett Vitaz <vitazbt@uw.edu>
2016-06-21 18:50:41 -0700

    fix unit tests for new functionality

--- src/ui/axis/test/axis.test.js
+++ src/ui/axis/test/axis.test.js
@@ -12,13 +12,21 @@ describe('<Axis />', () => {
   const dummyScale = d3Scale.scaleLinear();
 
   it('renders a g that wraps axis', () => {
-    const wrapper = shallow(<Axis scale={dummyScale} />);
+    const wrapper = shallow(
+      <Axis
+        scale={dummyScale}
+        orientation="bottom"
+      />);
     expect(wrapper)
       .to.have.tagName('g');
   });
 
   it('renders ticks', () => {
-    const wrapper = shallow(<Axis scale={dummyScale} />);
+    const wrapper = shallow(
+      <Axis
+        scale={dummyScale}
+        orientation="bottom"
+      />);
     expect(wrapper)
       .to.have.descendants('.tick');
 
@@ -28,7 +36,12 @@ describe('<Axis />', () => {
 
   it('modifies the output of d3-axis based on configuration', () => {
     const wrapper = shallow(
-      <Axis scale={dummyScale.range([0, 800])} ticks={6} tickValues={[1, 2, 3, 4, 5, 6]} />
+      <Axis
+        scale={dummyScale.range([0, 800])}
+        orientation="bottom"
+        ticks={6}
+        tickValues={[1, 2, 3, 4, 5, 6]}
+      />
     );
     expect(wrapper.find('.tick')).to.have.length(6);
     expect(wrapper.find('.tick').first().find('text')).to.have.text('1.0');
@@ -37,9 +50,12 @@ describe('<Axis />', () => {
 
   it('applies style to the g element', () => {
     const wrapper = shallow(
-      <Axis scale={dummyScale} style={{ stroke: 'red' }} />
+      <Axis
+        scale={dummyScale}
+        orientation="bottom"
+        style={{ stroke: 'red' }}
+      />
     );
-
-    expect(wrapper).to.have.attr('style', 'stroke:red;');
+    expect(wrapper.children().first()).to.have.attr('style', 'stroke:red;');
   });
 });
