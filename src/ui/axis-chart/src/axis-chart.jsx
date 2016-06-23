3ca045f3c284a9fb61b3d2e6e2b9ce9db102c970
Brett Vitaz <vitazbt@uw.edu>
2016-06-23 13:53:53 -0700

    inline `propTypes`

--- src/ui/axis-chart/src/axis-chart.jsx
+++ src/ui/axis-chart/src/axis-chart.jsx
@@ -4,54 +4,6 @@ import { getScale, getScaleTypes } from '../../../utils';
 
 const SCALE_TYPES = getScaleTypes();
 
-const propTypes = {
-  /* extra class names to appended to the element */
-  extraClasses: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.string,
-    PropTypes.object
-  ]),
-
-  /* [min, max] for xScale (i.e., the domain of the data) */
-  xDomain: PropTypes.array,
-
-  /* type of scale */
-  xScaleType: PropTypes.oneOf(SCALE_TYPES),
-
-  /* [min, max] yScale (i.e., the range of the data) */
-  yDomain: PropTypes.array,
-
-  /* type of scale */
-  yScaleType: PropTypes.oneOf(SCALE_TYPES),
-
-  /* px width of line chart */
-  width: PropTypes.number,
-
-  /* px height of line chart */
-  height: PropTypes.number,
-
-  /* margins to subtract from width and height */
-  margins: PropTypes.shape({
-    top: PropTypes.number,
-    bottom: PropTypes.number,
-    right: PropTypes.number,
-    left: PropTypes.number
-  }),
-
-  children: PropTypes.node,
-};
-
-const defaultProps = {
-  extraClasses: [],
-  margins: {
-    top: 20,
-    right: 20,
-    bottom: 30,
-    left: 50
-  },
-  xScaleType: 'linear'
-};
-
 export function calcDimensions(width, height, margins) {
   return {
     width: width - (margins.left + margins.right),
@@ -98,6 +50,50 @@ export default class AxisChart extends React.Component {
   }
 }
 
-AxisChart.propTypes = propTypes;
+AxisChart.propTypes = {
+  /* extra class names to appended to the element */
+  extraClasses: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.string,
+    PropTypes.object
+  ]),
 
-AxisChart.defaultProps = defaultProps;
+  /* [min, max] for xScale (i.e., the domain of the data) */
+  xDomain: PropTypes.array,
+
+  /* type of scale */
+  xScaleType: PropTypes.oneOf(SCALE_TYPES),
+
+  /* [min, max] yScale (i.e., the range of the data) */
+  yDomain: PropTypes.array,
+
+  /* type of scale */
+  yScaleType: PropTypes.oneOf(SCALE_TYPES),
+
+  /* px width of line chart */
+  width: PropTypes.number,
+
+  /* px height of line chart */
+  height: PropTypes.number,
+
+  /* margins to subtract from width and height */
+  margins: PropTypes.shape({
+    top: PropTypes.number,
+    bottom: PropTypes.number,
+    right: PropTypes.number,
+    left: PropTypes.number
+  }),
+
+  children: PropTypes.node,
+};
+
+AxisChart.defaultProps = {
+  extraClasses: [],
+  margins: {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 50
+  },
+  xScaleType: 'linear'
+};
