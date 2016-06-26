50f7c5e7143f7d6d59b71f1d7aaa279405e22272
Brett Vitaz <vitazbt@uw.edu>
2016-06-25 17:09:53 -0700

    add optional ticks to `<Track />`

--- src/ui/slider/src/track.jsx
+++ src/ui/slider/src/track.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import interact from 'interact.js';
 import isEmpty from 'lodash/isEmpty';
+import map from 'lodash/map';
 import { CommonPropTypes, PureComponent } from '../../../utils';
 
 import { getSnapTargetFunc } from './util';
@@ -10,13 +11,25 @@ import style from './style.css';
 export default class Track extends PureComponent {
   constructor(props) {
     super(props);
+    this.state = {};
     this.trackRef = this.trackRef.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
+    const state = {};
+
     if (this.props.snapTarget !== newProps.snapTarget) {
       this.bindInteract(newProps.snapTarget);
+
+      if (newProps.ticks) {
+        state.ticks = [];
+        for (let x = newProps.snapTarget.x; x < this.width; x += newProps.snapTarget.x) {
+          state.ticks.push(x);
+        }
+      }
     }
+
+    this.setState(state);
   }
 
   componentWillUnmount() {
@@ -48,11 +61,30 @@ export default class Track extends PureComponent {
     this._track = ref;
   }
 
+  renderTicks() {
+    if (!this.state.ticks) return null;
+
+    return (
+      <svg className={style['track-tick']} width="100%" height="100%">
+        {
+          map(this.state.ticks, (x) => {
+            return (
+              <g key={x}>
+                <line x1={x} x2={x} y1="0%" y2="100%" stroke="black" />
+              </g>
+            );
+          })
+        }
+      </svg>
+    );
+  }
+
   render() {
     return (
       <div className={classNames(style.track, this.props.className)} style={this.props.style}>
         <div ref={this.trackRef} className={style['track-click-target']}></div>
         {this.props.children}
+        {this.renderTicks()}
       </div>
     );
   }
@@ -67,4 +99,5 @@ Track.propTypes = {
     PropTypes.func,
     PropTypes.object,
   ]).isRequired,
+  ticks: PropTypes.bool,
 };
