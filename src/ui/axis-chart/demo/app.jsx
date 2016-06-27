ed4eba9abbcccffefef205919889b0971658b926
Brett Vitaz <vitazbt@uw.edu>
2016-06-27 16:30:03 -0700

    lint and add button to test prop changes

--- src/ui/axis-chart/demo/app.jsx
+++ src/ui/axis-chart/demo/app.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import { maxBy, minBy, map, uniqBy } from 'lodash';
-
 import d3Scale from 'd3-scale';
 
 import { dataGenerator } from '../../../test-utils';
 import AxisChart from '../';
 import { MultiLine } from '../../shape';
 import { XAxis, YAxis } from '../../axis';
+import Button from '../../button';
 
 const keyField = 'year_id';
 const valueField = 'value';
 
 const data = dataGenerator({
-  primaryKeys: [{name: 'location', values: ['A', 'B', 'C', 'D', 'E', 'F']}],
-  valueKeys: [
-    {name: valueField, range: [200, 500], uncertainty: true}
-  ],
-  length: 10
+  primaryKeys: [{ name: 'location', values: ['A', 'B', 'C', 'D', 'E', 'F'] }],
+  valueKeys: [{ name: valueField, range: [200, 500], uncertainty: true }],
+  length: 10,
 });
 
 const dataA = data.filter((d) => {
@@ -51,54 +48,76 @@ const lineData = [
   { location: 'C', values: dataC },
   { location: 'D', values: dataD },
   { location: 'E', values: dataE },
-  { location: 'F', values: dataF }
+  { location: 'F', values: dataF },
 ];
 
 const yDomain = [minBy(data, 'value_lb').value_lb, maxBy(data, 'value_ub').value_ub];
 const xDomain = map(uniqBy(data, keyField), (obj) => { return (obj[keyField]); });
-const colorScale = d3Scale.scaleOrdinal().domain(['A', 'B', 'C', 'D', 'E', 'F']).range(['red', 'blue', 'orange', 'green', 'salmon', 'violet']);
+const colorScale = d3Scale.scaleOrdinal().domain(['A', 'B', 'C', 'D', 'E', 'F'])
+  .range(['red', 'blue', 'orange', 'green', 'salmon', 'violet']);
 
 const dims = {
+  width: 800,
   height: 600,
-  width: 800
 };
 
-const padding = {
-  top: 20,
-  bottom: 40,
-  left: 55,
-  right: 20,
-};
+const padding = { top: 20, bottom: 40, left: 55, right: 20 };
 
 const axisStyle = {
   fontFamily: 'sans-serif',
-  fontSize: '11px'
+  fontSize: '11px',
 };
 
+const dataAccessors = { x: keyField, y: valueField, y0: 'value_lb', y1: 'value_ub' };
+const chartClassName = ['foo', 'bar'];
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { render: 0 };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      render: this.state.render + 1,
+      xScaleType: 'linear',
+      xDomain: [Math.min(...xDomain), Math.max(...xDomain)],
+      width: 600,
+    });
+  }
+
   render() {
     return (
-      <AxisChart
-        width={dims.width}
-        height={dims.height}
-        padding={padding}
-        xDomain={xDomain}
-        xScaleType="point"
-        yDomain={yDomain}
-        yScaleType="linear"
-        className={['foo', 'bar']}
-      >
-        <XAxis style={axisStyle} label="Year" />
-        <YAxis style={axisStyle} label="Probability" />
-        <MultiLine
-          data={lineData}
-          keyField={'location'}
-          dataField={'values'}
-          colorScale={colorScale}
-          showUncertainty
-          dataAccessors={{ x: keyField, y: valueField, y0: 'value_lb', y1: 'value_ub' }}
-        />
-      </AxisChart>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div>
+          <Button
+            text="Resize and change scale"
+            clickHandler={this.onClick}
+          />
+        </div>
+        <AxisChart
+          width={this.state.width || dims.width}
+          height={dims.height}
+          padding={padding}
+          xDomain={this.state.xDomain || xDomain}
+          xScaleType={this.state.xScaleType || 'point'}
+          yDomain={yDomain}
+          yScaleType="linear"
+          className={chartClassName}
+        >
+          <XAxis style={axisStyle} label="Year" />
+          <YAxis style={axisStyle} label="Probability" />
+          <MultiLine
+            data={lineData}
+            keyField={'location'}
+            dataField={'values'}
+            colorScale={colorScale}
+            showUncertainty
+            dataAccessors={dataAccessors}
+          />
+        </AxisChart>
+      </div>
     );
   }
 }
