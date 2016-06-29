5c85bab640b3a64b0a76f4c4d9a4ab4c6a77caac
Brett Vitaz <vitazbt@uw.edu>
2016-06-28 18:09:18 -0700

    add prop validation utility `atLeastOneOfProp` for those times where you can have more than one

--- src/utils/index.js
+++ src/utils/index.js
@@ -43,6 +43,7 @@ export {
 
 export {
   CommonPropTypes,
+  atLeastOneOfProp,
   oneOfProp,
   propsChanged,
 } from './props';
--- src/utils/props.js
+++ src/utils/props.js
@@ -28,6 +28,23 @@ export function oneOfProp(propTypes) {
   };
 }
 
+export function atLeastOneOfProp(propTypes) {
+  return (props, propName, componentName, location, propFullName) => {
+    let error = null;
+    const validProps = intersection(Object.keys(props), Object.keys(propTypes));
+    if (validProps.length > 0) {
+      if (includes(validProps, propName)) {
+        error = propTypes[propName](props, propName, componentName, location, propFullName);
+      }
+    } else {
+      /* eslint-disable max-len */
+      error = `At least one of prop [\`${Object.keys(propTypes).join('`,`')}\`] must be specified in \`${componentName}\`.`;
+      /* eslint-enable max-len */
+    }
+    return error && new Error(error);
+  };
+}
+
 export function propsChanged(prevProps, nextProps, propsToCompare, propsToOmit) {
   return !reduce(propsToCompare || Object.keys(nextProps), (acc, prop) => {
     return acc && (includes(propsToOmit, prop) || prevProps[prop] === nextProps[prop]);
