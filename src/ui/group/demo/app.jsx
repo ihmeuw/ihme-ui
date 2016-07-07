30fd0201ff3edf48588e8c195846fc213a962f00
Brett Vitaz <vitazbt@uw.edu>
2016-07-07 15:13:09 -0700

    lint, improve efficiency (correctness)

--- src/ui/group/demo/app.jsx
+++ src/ui/group/demo/app.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import concat from 'lodash/concat';
+import { PureComponent } from '../../../utils';
 
-import { concat } from 'lodash';
-
-import Group from '../';
-import { Option } from '../';
-
+import Group, { Option } from '../';
 
 const data = [
   { name: 'males', value: '1' },
   { name: 'females', value: '2' },
-  { name: 'both', value: '3' }
+  { name: 'both', value: '3' },
 ];
 
-class App extends React.Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
 
@@ -54,4 +52,4 @@ class App extends React.Component {
   }
 }
 
-render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
