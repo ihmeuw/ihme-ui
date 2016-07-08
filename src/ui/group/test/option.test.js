6d2dec3a582941ba74f9fd7bfa475dda3eb0a13c
Brett Vitaz <vitazbt@uw.edu>
2016-07-07 17:56:21 -0700

    add tests for `disabledStyle` and `disabledClassName`

--- src/ui/group/test/option.test.js
+++ src/ui/group/test/option.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
 import chai, { expect } from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 import { shallow } from 'enzyme';
 
 import { Option } from '../';
+import styles from '../src/group.css';
 
 chai.use(chaiEnzyme());
 
@@ -13,5 +13,49 @@ describe('<Option />', () => {
     const wrapper = shallow(<Option key={1} text="One" value="1" />);
 
     expect(wrapper).to.have.exactly(1).descendants('Button');
+  });
+
+  describe('styles', () => {
+    it('applies disabled styles, when disabled', () => {
+      const wrapper = shallow(
+        <Option disabledStyle={{ opacity: '0.5' }} disabled />
+      );
+      expect(wrapper).to.have.style('opacity', '0.5');
+    });
+
+    it('does not apply disabled styles, when not disabled', () => {
+      const wrapper = shallow(
+        <Option disabledStyle={{ opacity: '0.5' }} />
+      );
+      expect(wrapper).to.not.have.style('opacity', '0.5');
+    });
+
+    it('applies disabled styles, when disabled prop is changed', () => {
+      const wrapper = shallow(
+        <Option disabledStyle={{ opacity: '0.5' }} />
+      );
+      expect(wrapper).to.not.have.style('opacity', '0.5');
+      wrapper.setProps({ disabled: true });
+      expect(wrapper).to.have.style('opacity', '0.5');
+      wrapper.setProps({ disabled: false });
+      expect(wrapper).to.not.have.style('opacity', '0.5');
+    });
+  });
+
+  describe('classNames', () => {
+    it('applies default disabled style, when disabled', () => {
+      const wrapper = shallow(
+        <Option disabled />
+      );
+      expect(wrapper).to.have.className(styles.disabled);
+    });
+
+    it('applies provided disabled style, when disabled', () => {
+      const wrapper = shallow(
+        <Option disabledClassName="disabled-test" disabled />
+      );
+      expect(wrapper).to.have.className('disabled-test');
+      expect(wrapper).to.not.have.className(styles.disabled);
+    });
   });
 });
