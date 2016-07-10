d45cd3ea6b768e4b83ce40cbb6e1f8524c018f73
Brett Vitaz <vitazbt@uw.edu>
2016-07-10 10:46:15 -0700

    begin refactor to allow `restore` transitions. fix flex style passing due to FireFox stupidness. use `transitionend` events where transitions are used. NOTE: FireFox fails to transition on restore, Edge fails to transition randomly, Chrome and Safari are OK!

--- src/ui/expansion-container/src/Expandable.jsx
+++ src/ui/expansion-container/src/Expandable.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import bindAll from 'lodash/bindAll';
+import reduce from 'lodash/reduce';
 import pick from 'lodash/pick';
 import { CommonPropTypes, PureComponent } from '../../../utils';
 import { getBackgroundColor } from '../../../utils/window';
@@ -8,7 +9,30 @@ import { getBackgroundColor } from '../../../utils/window';
 import { containerStore } from './ExpansionContainer';
 import styles from './expansion-container.css';
 
-const LAYOUT_SELECTORS = ['display', 'flexFlow', 'justifyContent', 'alignItems', 'alignContent'];
+const transitionTypes = {
+  transition: 'transitionend',
+  MozTransition: 'transitionend',
+  OTransition: 'oTransitionEnd',
+  WebkitTransition: 'webkitTransitionEnd',
+};
+
+/* From Modernizr */
+export const transitionEvent = (() => {
+  const el = document.createElement('transitionEventTest');
+  return reduce(transitionTypes, (acc, type, t) => {
+    return acc || (el.style[t] !== undefined ? type : undefined);
+  }, undefined);
+})();
+
+const LAYOUT_SELECTORS = [
+  'alignContent',
+  'alignItems',
+  'display',
+  'flexDirection',
+  /* 'flexFlow', */ // causes firefox to fail flex layout
+  'flexWrap',
+  'justifyContent',
+];
 
 export default class Expandable extends PureComponent {
   constructor(props) {
@@ -16,9 +40,12 @@ export default class Expandable extends PureComponent {
     this._expansionContainer = containerStore[props.group];
     this._expansionContainer.subscribe(this);
     this.state = {
-      hidden: false,
       expanded: false,
       expanding: false,
+      hidden: false,
+      restoring: false,
+      restored: true,
+      transitioning: false,
       outerStyle: props.style,
       iconStyle: {
         position: 'absolute',
@@ -33,11 +60,13 @@ export default class Expandable extends PureComponent {
       'onHide',
       'onRestore',
       'onResize',
+      'onTransitionEnd',
       'expand',
       'hide',
       'restore',
       'containerRef',
       'setState',
+      'innerRef',
     ]);
   }
 
@@ -45,13 +74,14 @@ export default class Expandable extends PureComponent {
     this.backgroundColor = getBackgroundColor(this._container);
     this.outerStyle = {
       ...this.props.style,
-      display: 'initial',
-      flexFlow: 'initial',
-      justifyContent: 'initial',
-      alignItems: 'initial',
-      alignContent: 'initial',
+      display: undefined,
+      flexFlow: undefined,
+      justifyContent: undefined,
+      alignItems: undefined,
+      alignContent: undefined,
     };
     this.innerStyle = {
+      ...this.props.expandableStyle,
       ...pick(this.containerStyle, LAYOUT_SELECTORS),
     };
     this.defaultState = {
@@ -67,23 +97,31 @@ export default class Expandable extends PureComponent {
   }
 
   componentDidUpdate() {
-    if (this.state.expanding) setTimeout(this.setState, 0, { expanding: false });
+    if (this.state.transitioning && !!this.state.innerStyle.transition) {
+      // do nothing
+    } else if (this.state.expanding) {
+      if (this.state.expanded) {
+        setTimeout(this.setState, 0, {
+          expanding: false,
+          transitioning: false,
+        });
+      }
+    } else if (this.state.restoring) {
+      if (!this.state.restored) {
+        setTimeout(this.setState, 0, this.defaultState);
+      }
+    }
   }
 
   onExpand() {
-    const { left, top, width, height } = this.boundingClientRect;
-
     this.setState({
-      hidden: false,
-      expanded: true,
+      expanded: false,
       expanding: true,
-      innerStyle: {
-        ...this.state.innerStyle,
-        position: 'fixed',
-        left, top, width, height,
-        zIndex: '1',
-        backgroundColor: this.backgroundColor,
-      },
+      hidden: false,
+      restoring: false,
+      restored: false,
+      transitioning: false,
+      innerStyle: this.calcInnerStyle(this.boundingClientRect),
     });
   }
 
@@ -94,17 +132,57 @@ export default class Expandable extends PureComponent {
   }
 
   onRestore() {
-    this.setState(this.defaultState);
+    if (this.state.expanded) {
+      this.setState({
+        expanded: false,
+        expanding: false,
+        hidden: false,
+        restored: false,
+        restoring: true,
+        transitioning: !!this.props.transition,
+        innerStyle: {
+          ...this.calcInnerStyle(this.boundingClientRect),
+          transition: this.props.transition,
+        },
+      });
+    } else {
+      this.setState(this.defaultState);
+    }
   }
 
-  onResize({ left, top, width, height }) {
-    setTimeout(this.setState, 0, {
+  onResize(boundingClientRect) {
+    const state = {
       innerStyle: {
-        ...this.state.innerStyle,
-        transition: this.state.expanding ? 'all 1s ease' : 'initial',
-        left, top, width, height,
+        ...this.calcInnerStyle(boundingClientRect),
+        transition: !this.state.expanded && this.props.transition,
       },
-    });
+      expanded: this.state.expanded || !this.props.transition,
+      expanding: !this.state.expanded && !!this.props.transition,
+      transitioning: !this.state.expanded && !!this.props.transition,
+    };
+    setTimeout(this.setState, 0, state);
+  }
+
+  onTransitionEnd() {
+    if (this.state.transitioning) {
+      this.setState({
+        expanded: !!this.state.expanding,
+        expanding: false,
+        transitioning: false,
+      });
+    }
+  }
+
+  calcInnerStyle({ left, top, width, height }) {
+    return {
+      ...this.innerStyle,
+      backgroundColor: this.backgroundColor,
+      left, top, width, height,
+      right: undefined,
+      bottom: undefined,
+      position: 'fixed',
+      zIndex: '1',
+    };
   }
 
   expand() {
@@ -135,6 +213,10 @@ export default class Expandable extends PureComponent {
     this._container = ref;
   }
 
+  innerRef(ref) {
+    if (ref) ref.addEventListener(transitionEvent, this.onTransitionEnd);
+  }
+
   renderExpandIcon(hideIcon) {
     if (hideIcon) return null;
     const {
@@ -142,6 +224,7 @@ export default class Expandable extends PureComponent {
     } = this.props;
     const {
       expanded,
+      expanding,
       iconStyle,
     } = this.state;
 
@@ -154,7 +237,7 @@ export default class Expandable extends PureComponent {
         width="1em" height="1em"
       >
         <circle r="15" fill="none" stroke="black" />
-        {!!expanded ? (
+        {(expanding || expanded) ? (
           <path d="M1,-10.6 L10.6,-1 L1,-1z M-10.6,1 L-1,10.6 L-1,1z" />
         ) : (
           <path d="M6.4,3.2 L-3.2,-6.4 L6.4,-6.4z M3.2,6.4 L-6.4,-3.2 L-6.4,6.4z" />
@@ -167,6 +250,7 @@ export default class Expandable extends PureComponent {
     const {
       children,
       className,
+      expandableClassName,
       hideIcon,
     } = this.props;
     const {
@@ -182,7 +266,8 @@ export default class Expandable extends PureComponent {
       >
         {!!innerStyle && (
           <div
-            className={styles['expandable-parent']}
+            ref={this.innerRef}
+            className={classNames(styles['expandable-parent'], expandableClassName)}
             style={innerStyle}
           >
                {children}
@@ -205,8 +290,15 @@ Expandable.propTypes = {
   group: PropTypes.string,
   hideIcon: PropTypes.bool,
   expanded: PropTypes.bool,
+  transition: PropTypes.string,
 };
 
 Expandable.defaultProps = {
   group: 'default',
+  iconStyle: {
+    position: 'absolute',
+    top: '0.2em',
+    right: '0.2em',
+  },
+  // transition: 'all 0.5s ease',
 };
