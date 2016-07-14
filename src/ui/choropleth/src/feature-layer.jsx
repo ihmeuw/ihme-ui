8ca1405efc6a03574e2225d9a4f633f7962fe26b
Gabe Medrash <gmedrash@gmail.com>
2016-07-14 16:43:39 -0700

    props management

--- src/ui/choropleth/src/feature-layer.jsx
+++ src/ui/choropleth/src/feature-layer.jsx
@@ -1,22 +1,30 @@
 import React, { PropTypes } from 'react';
 import { keyBy, map } from 'lodash';
 
+import {
+  CommonPropTypes,
+} from '../../../utils';
+
 import Path from './path';
 
 export default function FeatureLayer(props) {
   const {
-    features,
-    pathGenerator,
     colorScale,
     data,
+    features,
     keyField,
-    valueField,
-    selectedLocations,
     onClick,
     onMouseOver,
     onMouseMove,
     onMouseDown,
-    onMouseOut
+    onMouseOut,
+    pathGenerator,
+    pathClassName,
+    pathSelectedClassName,
+    pathSelectedStyle,
+    pathStyle,
+    selectedLocations,
+    valueField,
   } = props;
 
   // optimization: turn array of ids into map keyed by those ids
@@ -36,21 +44,21 @@ export default function FeatureLayer(props) {
 
           return (
             <Path
+              className={pathClassName}
               key={key}
               feature={feature}
-              pathGenerator={pathGenerator}
-              locationId={key}
               fill={fill}
-              selected={!!selectedLocationsMappedById[key]}
+              locationId={key}
               onClick={onClick}
               onMouseOver={onMouseOver}
               onMouseMove={onMouseMove}
               onMouseDown={onMouseDown}
               onMouseOut={onMouseOut}
-              className={props.pathClassName}
-              selectedClassName={props.pathSelectedClassName}
-              style={props.pathStyle}
-              selectedStyle={props.pathSelectedStyle}
+              pathGenerator={pathGenerator}
+              selected={!!selectedLocationsMappedById[key]}
+              selectedClassName={pathSelectedClassName}
+              selectedStyle={pathSelectedStyle}
+              style={pathStyle}
             />
           );
         })
@@ -60,54 +68,59 @@ export default function FeatureLayer(props) {
 }
 
 FeatureLayer.propTypes = {
-  /* array of geoJSON feature objects, e.g.: [{ geometry: [Object], properties: [Object] }] */
-  features: PropTypes.arrayOf(PropTypes.object).isRequired,
+  /* fn that accepts keyfield, and returns fill color for Path */
+  colorScale: PropTypes.func.isRequired,
 
   /*
-    unlike in other components, data should be an object keyed by location id
-    this allows quick lookup of a location's value
-    e.g. { 6: { mean: 2, lb: 1, ub: 3, ... }, 570: { mean: 4, lb: 3, ub: 5, ... } }
-  */
+   unlike in other components, data should be an object keyed by location id
+   this allows quick lookup of a location's value
+   e.g. { 6: { mean: 2, lb: 1, ub: 3, ... }, 570: { mean: 4, lb: 3, ub: 5, ... } }
+   */
   data: PropTypes.object.isRequired,
+
+  /* array of geoJSON feature objects, e.g.: [{ geometry: [Object], properties: [Object] }] */
+  features: PropTypes.arrayOf(PropTypes.object).isRequired,
 
   /* mapping of datum key field to geoJSON feature key. default: 'id' */
   keyField: PropTypes.string,
 
-  /* key of datum that holds the value to display */
-  valueField: PropTypes.string,
+  /* passed to each path; signature: function(event, locationId, Path) {...} */
+  onClick: PropTypes.func,
+
+  /* passed to each path; signature: function(event, locationId, Path) {...} */
+  onMouseOver: PropTypes.func,
+
+  /* passed to each path; signature: function(event, locationId, Path) {...} */
+  onMouseMove: PropTypes.func,
+
+  /* passed to each path; signature: function(event, locationId, Path) {...} */
+  onMouseDown: PropTypes.func,
+
+  /* passed to each path; signature: function(event, locationId, Path) {...} */
+  onMouseOut: PropTypes.func,
+
+  pathClassName: CommonPropTypes.className,
 
   /* function to generate `d` attribute of <path> elements */
   pathGenerator: PropTypes.func.isRequired,
 
-  /* fn that accepts keyfield, and returns fill color for Path */
-  colorScale: PropTypes.func.isRequired,
+  pathSelectedClassName: CommonPropTypes.className,
+
+  /* selected style object or function to pass to each path; if a function, receives feature as arg */
+  pathSelectedStyle: CommonPropTypes.style,
+
+  /* base style object or function to pass to each path; if a function, receives feature as arg */
+  pathStyle: CommonPropTypes.style,
 
   /* array of datum[keyField], e.g., location ids */
   selectedLocations: PropTypes.arrayOf(PropTypes.number),
 
-  /* passed to each path; signature: function(event, locationId) {...} */
-  onClick: PropTypes.func,
-
-  /* passed to each path; signature: function(event, locationId) {...} */
-  onMouseOver: PropTypes.func,
-
-  /* passed to each path; signature: function(event, locationId) {...} */
-  onMouseMove: PropTypes.func,
-
-  /* passed to each path; signature: function(event, locationId) {...} */
-  onMouseDown: PropTypes.func,
-
-  /* passed to each path; signature: function(event, locationId) {...} */
-  onMouseOut: PropTypes.func,
-
-  pathClassName: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  pathSelectedClassName: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  pathStyle: PropTypes.object,
-  pathSelectedStyle: PropTypes.object,
+  /* key of datum that holds the value to display */
+  valueField: PropTypes.string,
 };
 
 FeatureLayer.defaultProps = {
-  keyField: 'id',
   dataField: 'mean',
+  keyField: 'id',
   selectedLocations: []
 };
