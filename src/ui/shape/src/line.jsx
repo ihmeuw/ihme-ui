49ce00b61514ac04f7e5af8324e51f95dba27bed
davschne <davschne@uw.edu>
2016-08-19 16:59:37 -0700

    Convert <Line> to class extending PureComponent.

--- src/ui/shape/src/line.jsx
+++ src/ui/shape/src/line.jsx
@@ -4,6 +4,7 @@ import { line } from 'd3-shape';
 
 import { noop } from 'lodash';
 
+import { PureComponent } from '../../../utils';
 import { eventHandleWrapper } from '../../../utils/events';
 
 const propTypes = {
@@ -52,45 +53,45 @@ const defaultProps = {
   onMouseLeave: noop,
 };
 
-const Line = (props) => {
-  const {
-    data,
-    scales,
-    fill,
-    stroke,
-    strokeWidth,
-    dataAccessors: { x: xAccessor, y: yAccessor },
-    onClick,
-    onMouseLeave,
-    onMouseMove,
-    onMouseOver,
-  } = props;
+export default class Line extends PureComponent {
+  render() {
+    const {
+      data,
+      scales,
+      fill,
+      stroke,
+      strokeWidth,
+      dataAccessors: { x: xAccessor, y: yAccessor },
+      onClick,
+      onMouseLeave,
+      onMouseMove,
+      onMouseOver,
+    } = this.props;
 
-  const path = line()
-    .x((datum) => {
-      return scales.x(datum[xAccessor]);
-    })
-    .y((datum) => {
-      return scales.y(datum[yAccessor]);
-    });
+    const path = line()
+      .x((datum) => {
+        return scales.x(datum[xAccessor]);
+      })
+      .y((datum) => {
+        return scales.y(datum[yAccessor]);
+      });
 
-  return (
-    <path
-      className="line"
-      fill={fill}
-      stroke={stroke}
-      strokeWidth={`${strokeWidth}px`}
-      d={path(data)}
-      onClick={eventHandleWrapper(onClick, data, this)}
-      onMouseLeave={eventHandleWrapper(onMouseLeave, data, this)}
-      onMouseMove={eventHandleWrapper(onMouseMove, data, this)}
-      onMouseOver={eventHandleWrapper(onMouseOver, data, this)}
-    />
-  );
-};
+    return (
+      <path
+        className="line"
+        fill={fill}
+        stroke={stroke}
+        strokeWidth={`${strokeWidth}px`}
+        d={path(data)}
+        onClick={eventHandleWrapper(onClick, data, this)}
+        onMouseLeave={eventHandleWrapper(onMouseLeave, data, this)}
+        onMouseMove={eventHandleWrapper(onMouseMove, data, this)}
+        onMouseOver={eventHandleWrapper(onMouseOver, data, this)}
+      />
+    );
+  }
+}
 
 Line.propTypes = propTypes;
 
 Line.defaultProps = defaultProps;
-
-export default Line;
