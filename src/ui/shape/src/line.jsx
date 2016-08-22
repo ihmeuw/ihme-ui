43344d239aa0861ed616d2d8ed6cc41a3886565c
davschne <davschne@uw.edu>
2016-08-22 12:45:07 -0700

    Require x, y properties of props.dataAccessors.

--- src/ui/shape/src/line.jsx
+++ src/ui/shape/src/line.jsx
@@ -22,8 +22,8 @@ const propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
 
   dataAccessors: PropTypes.shape({
-    x: PropTypes.oneOf([PropTypes.string, PropTypes.func]),
-    y: PropTypes.oneOf([PropTypes.string, PropTypes.func]),
+    x: PropTypes.oneOf([PropTypes.string, PropTypes.func]).isRequired,
+    y: PropTypes.oneOf([PropTypes.string, PropTypes.func]).isRequired,
   }).isRequired,
 
   fill: PropTypes.string,
