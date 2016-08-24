8af7f2b0a355206036a083d6156034de2bc1ca38
Brett Vitaz <vitazbt@uw.edu>
2016-08-23 23:43:42 -0700

    new spinner style and pulse timing. use `svg` instead of `span` to keep the spinner dots consistently round at all sizes. use `PureComponent` and add `style` prop.

--- src/ui/spinner/src/spinner.jsx
+++ src/ui/spinner/src/spinner.jsx
@@ -1,37 +1,42 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
+import { Symbol } from '../../shape';
+import { CommonPropTypes, PureComponent } from '../../../utils';
 
 import styles from './spinner.css';
 
-const propTypes = {
+class Spinner extends PureComponent {
+  render() {
+    const { props } = this;
+    return (
+      <div
+        className={classNames(styles.spinner, {
+          [styles['inline-spinner']]: props.inline
+        }, props.className)}
+        style={props.style}
+      >
+        <svg className={styles.dot} viewBox="-8 -8 16 16" width="1em" height="1em">
+          <Symbol symbolType="circle" fill="black" />
+        </svg>
+        <svg className={styles.dot} viewBox="-8 -8 16 16" width="1em" height="1em">
+          <Symbol symbolType="circle" fill="black" />
+        </svg>
+        <svg className={styles.dot} viewBox="-8 -8 16 16" width="1em" height="1em">
+          <Symbol symbolType="circle" fill="black" />
+        </svg>
+      </div>
+    );
+  }
+}
+
+Spinner.propTypes = {
+  /* an extra classname */
+  className: CommonPropTypes.className,
+
   /* display spinner inline with other elements (e.g., in a button) */
   inline: PropTypes.bool,
 
-  /* how large the dots will be */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-
-  /* an extra classname */
-  className: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.array,
-    PropTypes.object
-  ])
+  style: CommonPropTypes.style,
 };
-
-const Spinner = (props) => {
-  return (
-    <div
-      className={classNames(styles.spinner, {
-        [styles['inline-spinner']]: props.inline
-      }, props.className)}
-    >
-      <span className={styles[`${props.size}-dot`]}></span>
-      <span className={styles[`${props.size}-dot`]}></span>
-      <span className={styles[`${props.size}-dot`]}></span>
-    </div>
-  );
-};
-
-Spinner.propTypes = propTypes;
 
 export default Spinner;
