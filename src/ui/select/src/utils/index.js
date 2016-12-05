0758990e8330c603767778bdcdf1ca8a122efad9
Gabe Medrash <gmedrash@gmail.com>
2016-12-05 10:20:39 -0800

    remove magic number

--- src/ui/select/src/utils/index.js
+++ src/ui/select/src/utils/index.js
@@ -2,17 +2,25 @@ import { getStringWidth } from '../../../../utils';
 
 /**
  *
- * @param options {Array} -> array of option objects
- * @param labelKey {String} -> key on option objects holding options' label
- * @param hierarchical {Boolean} -> whether or not to take into account padding due to hierarchy
- * @returns {Number} pixel width of widest label
+ * @param {array} options - array of option objects
+ * @param {string} labelKey - key on option objects holding options' label
+ * @param {bool} hierarchical - whether or not to take into account padding due to hierarchy
+ * @param {number} [levelPadding] - optional padding (in px) to apply per level (only if hierarchical)
+ * @returns {number} - pixel width of widest label
  */
-export const getWidestLabel = (options = [], labelKey = '', hierarchical = false) => {
+export const getWidestLabel = (
+  options = [],
+  labelKey = '',
+  hierarchical = false,
+  levelPadding = 5
+) => {
   return options.reduce((maxWidth, option) => {
     const labelWidth = getStringWidth(option[labelKey]);
 
     // take padding into account for hierarchically displayed list
-    const fullWidth = hierarchical ? labelWidth + 5 * option.level : labelWidth;
+    const fullWidth = hierarchical
+      ? labelWidth + levelPadding * option.level
+      : labelWidth;
 
     return Math.max(maxWidth, fullWidth);
   }, 0);
