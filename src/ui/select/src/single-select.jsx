e8192418fd876f16f5b1f371bd2f76189a40b609
Aaron Keel <akeel@uw.edu>
2017-02-06 13:16:59 -0800

    add deprecation warning

--- src/ui/select/src/single-select.jsx
+++ src/ui/select/src/single-select.jsx
@@ -12,6 +12,10 @@ import { menuWrapper } from './menu';
 export default class SingleSelect extends PureComponent {
   constructor(props) {
     super(props);
+    console.warn(
+      'Deprecated: SingleSelect will not be available in future versions.'
+      + ' Please use Select.'
+    );
     this.state = stateFromPropUpdates(SingleSelect.propUpdates, {}, props, {});
   }
 
