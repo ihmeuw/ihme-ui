7588606ff00b23eebfc973ad94a5b105c23d71a8
Aaron Keel <akeel@uw.edu>
2017-02-23 09:55:29 -0800

    lint

--- src/ui/responsive-container/test/responsive-container.test.js
+++ src/ui/responsive-container/test/responsive-container.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import chai, { expect } from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 import { mount } from 'enzyme';
+import ResponsiveContainer from '../';
 
 chai.use(chaiEnzyme());
 
-import ResponsiveContainer from '../';
 
 /**
  * Until either JSDOM supports getBoundingClientRect or
