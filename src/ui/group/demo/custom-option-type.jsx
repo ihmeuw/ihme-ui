73414cfcdef43a16e41e034ae9bd456af8f047a9
Gabe Medrash <gmedrash@gmail.com>
2017-05-19 16:16:57 -0700

    beefier Group demo

---
+++ src/ui/group/demo/custom-option-type.jsx
@@ -0,0 +1,42 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+
+import { Button, Shape } from '../..';
+
+const SVG_STYLE = {
+  height: '100%',
+  width: '1em',
+};
+
+const TEXT_STYLE = {
+  margin: 0,
+};
+
+export default function CustomOptionType(props) {
+  return (
+    <Button
+      className={classNames(props.className)}
+      disabled={props.disabled}
+      onClick={props.onClick}
+      selected={props.selected}
+    >
+      <svg style={SVG_STYLE}>
+        <g>
+          <Shape
+            shapeType="diamond"
+            translateX={8}
+            translateY={8}
+          />
+        </g>
+      </svg>
+      <p style={TEXT_STYLE}>{props.text}</p>
+    </Button>
+  );
+}
+
+CustomOptionType.propTypes = {
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  selected: PropTypes.bool,
+  style: PropTypes.object,
+};
