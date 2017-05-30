c785a40d727eb4badee59e5dd4de23fd470cd6bf
James Vermillion <vermijw@uw.edu>
2017-05-30 09:55:59 -0700

    Added colorSteps array found in utils/colors.js to ChoroplethLegend.defaultProps

--- src/ui/choropleth-legend/src/choropleth-legend.jsx
+++ src/ui/choropleth-legend/src/choropleth-legend.jsx
@@ -315,6 +315,19 @@ ChoroplethLegend.defaultProps = {
     x: 0,
     y: 20
   },
+  colorSteps: [
+    '#a50026', // dark red
+    '#d73027',
+    '#f46d43',
+    '#fdae61',
+    '#fee090',
+    '#ffffbf', // light yellow
+    '#e0f3f8',
+    '#abd9e9',
+    '#74add1',
+    '#4575b4',
+    '#313695',  // dark blue
+  ],
   margins: {
     top: 50,
     right: 100,
