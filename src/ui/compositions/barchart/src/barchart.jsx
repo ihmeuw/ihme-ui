1fa29335c4f175e94872021f02f31c15aa263e7c
Bao Dinh <bdinh@D-108-179-189-54.dhcp4.washington.edu>
2017-07-20 13:27:02 -0700

    Adding demo for barchart component

--- src/ui/compositions/barchart/src/barchart.jsx
+++ src/ui/compositions/barchart/src/barchart.jsx
@@ -3,152 +3,98 @@ import classNames from 'classnames';
 import { scaleLinear, scaleBand } from 'd3';
 
 import {
-  castArray,
+  assign,
+  bindAll,
+  keyBy,
+  flatMap,
+  filter,
   get as getValue,
+  includes,
+  intersection,
+  intersectionWith,
+  isEqual,
   map,
+  toString,
 } from 'lodash';
-
-import {
-  isVertical,
-  combineStyles,
-  CommonDefaultProps,
-  CommonPropTypes,
-  memoizeByLastCall,
-  propResolver,
-  PureComponent,
-  stackedDataArray,
-} from '../../../../utils';
-
-import styles from './style.css';
-
-
-
 
 import AxisChart from './../../../axis-chart';
 import { XAxis, YAxis } from './../../../axis';
 import Bars from './../../../bar/src/bars';
 import ResponsiveContainer from '../../../responsive-container';
-import MultiBars from './../../../bar';
-import Legend from './../../../legend';
-
 
 export default class BarChart extends PureComponent {
   constructor(props) {
     super(props);
 
-    this.combineStyles = memoizeByLastCall(combineStyles);
-    this.castSelectionAsArray = memoizeByLastCall((selection) => castArray(selection));
 
   }
 
 
-  renderTitle() {
-    const {
-      title,
-      titleClassName,
-      titleStyle,
-    } = this.props;
-    if (!title) return null;
-    return (
-      <div className={classNames(styles.title, titleClassName)} style={titleStyle}>
-        <h2>
-          {title}
-        </h2>
-      </div>
-    )
+  calculateKeyDomain() {
+
   }
 
-  renderLegend() {
-    const {
-      legendObject,
-      labelKey,
-      shapeColorKey,
-      shapeTypeKey,
-      legendClassName,
-      legendStyle,
-    } = this.props;
+  calculateFieldDomain() {
 
-    return (
-      <div className={classNames(styles.legend, legendClassName)} style={legendStyle}>
-        <div className={styles['legend-wrapper']}>
-          {/*<ResponsiveContainer disableHeight>*/}
-            <Legend
-              items={legendObject}
-              labelKey={labelKey} // should default these to fit to one type of legend object config
-              shapeColorKey={shapeColorKey}
-              shapeTypeKey={shapeTypeKey}
-            />
-          {/*</ResponsiveContainer>*/}
-        </div>
-      </div>
-    );
+
   }
 
-  renderBarChart() {
-    const {
-      data,
-      className,
-      xDomain,
-      yDomain,
-      xScale,
-      yScale,
-      xLabel,
-      yLabel,
-      fill,
-      fillField,
-      stackField,
-      valueField,
-      barChartStyle
-    } = this.props;
 
 
 
-    return (
-      <div className={classNames(styles.barchart, barChartStyle)}>
-        {this.renderTitle()}
-        <ResponsiveContainer>
-          <AxisChart
-            xDomain={xDomain}
-            yDomain={yDomain}
-            xScaleType={xScale}
-            yScaleType={yScale}
-          >
-            <XAxis
-              label={xLabel} // should default to domain field
-            />
-            <YAxis
-              label={yLabel}
-            />
-            <Bars
-            data={data}
-            dataAccessors={{
-            fill: fillField,
-            key: 'id',
-            stack: stackField,    // year_id
-            value: valueField, // population
-            }}
-            // style={barChartStyle}
-            // selection={this.state.selectedItems}
-            >
-            </Bars>
-          </AxisChart>
-        </ResponsiveContainer>
-        {this.renderLegend()}
-      </div>
-    );
-  }
+  // renderBarChart() {
+  //   const {
+  //     data,
+  //     className,
+  //
+  //
+  //
+  //   } = this.props;
+  //
+  //   return (
+  //
+  //       <ResponsiveContainer>
+  //         <AxisChart height="" width=""/>
+  //
+  //
+  //       </ResponsiveContainer>
+  //
+  //   );
+  // }
 
   render() {
+    const {
+      data,
 
-    const {className, style, data} = this.props;
 
-    console.log(data);
+
+
+    } = this.props;
 
     return(
-      <div className={classNames(styles['barchart-container'], className)} style={style}>
+      <div className={className(styles['whatever styles goes here'], className)}
+           style={style}
+      >
         {/*{this.renderTitle()}*/}
-        {this.renderBarChart()}
-        {/*{this.renderLegend()}*/}
+        {/*{this.renderBarchart()}*/}
+        {/*{this.renderLegden()}*/}
+        <ResponsiveContainer>
+          <AxisChart
+            xDomain={}
+            yDomain={}
+            xScaleType={}
+            yscaletype={}
+          >
+            <XAxis/>
+            <YAxis/>
+            <Bars
+
+              data=""
+              dataAccessors=""/>
+
+
+          </AxisChart>
+        </ResponsiveContainer>
       </div>
     );
   }
