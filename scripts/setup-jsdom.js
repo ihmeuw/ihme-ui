393a56f12c583eeab52bb9484dff5b105322d90f
Katherine Beame <kbeame@uw.edu>
2017-12-04 14:54:35 -0800

    Update react to 16 and test dependencies to be compatible

--- scripts/setup-jsdom.js
+++ scripts/setup-jsdom.js
@@ -1,4 +1,8 @@
 import { JSDOM } from 'jsdom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>', {
   useAgent: 'node.js',
