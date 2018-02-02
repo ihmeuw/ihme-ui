f7da69b8ab5eb934b716beed21cb6dc2dc8869d6
Aaron Keel <akeel@uw.edu>
2018-02-02 15:28:49 -0800

    update webpack configs

--- webpack.demo.config.js
+++ webpack.demo.config.js
@@ -1,23 +1,17 @@
 'use strict';
 
-var path = require('path');
-var _ = require('lodash');
-var autoprefixer = require('autoprefixer');
+const path = require('path');
+const _ = require('lodash');
+const autoprefixer = require('autoprefixer');
 
-var baseConfig = require('./webpack.base.config');
-
-function customizer(objValue, srcValue) {
-  if (_.isArray(objValue)) {
-    return objValue.concat(srcValue);
-  }
-};
+const baseConfig = require('./webpack.base.config');
 
 module.exports = function(directory) {
   const demoConfig = {
     devtool: 'source-map',
     entry: path.resolve(directory, 'app.jsx'),
     output: {
-      path: directory,
+      path: path.resolve(directory),
       filename: 'bundle.js',
     },
     module: {
