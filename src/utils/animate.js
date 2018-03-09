3ad8b281fbb4da0a32959be505d4c5f0f6778eb1
James Vermillion <vermijw@uw.edu>
2018-03-09 15:34:23 -0800

    react-move-scatter: Add animation factory.
    - This refactor moves functionality for animation data processing to the animate.js util.
    - It also makes the processDatum functionality a factory as well since moving the animation functionality out of scope (to be used by other animatable components) makes a static nature a necessity.
    - factories used in scatter are updated in Scatter.propUpdates

---
+++ src/utils/animate.js
@@ -0,0 +1,79 @@
+import includes from 'lodash/includes';
+import get from 'lodash/get';
+import reduce from 'lodash/reduce';
+
+/**
+ *
+ * @param animate {}
+ * @param defaultProcessor
+ * @param animatableKeys
+ * @return {function(*=)}
+ */
+export function animationProcessorFactory(animate, defaultProcessor, animatableKeys) {
+  // A factory for each animation method: `start` | `enter` | `update` | `leave`;
+  return (method) => {
+    // Animation Processor function.
+    return (datum, index) => {
+      const {
+        events,
+        timing,
+        ...specificAnimationProps,
+      } = animate;
+
+      // Process datum, apply default animation, which can be overridden by user methods.
+      return reduce(
+        defaultProcessor(datum, index),
+        (accum, value, key) => {
+          if (includes(animatableKeys, key)) {
+            // A user defined animation method. ie, `animate.fill.update`
+            const userMethod = get(specificAnimationProps, [key, method]);
+
+            // Apply animate defaults that can be overridden by user for respective `key`.
+            const animationObject = {
+              [key]: [value],
+              events,
+              timing,
+              ...(userMethod && userMethod(value, datum, index)),
+            };
+
+            // Concatenate with accumulator and return.
+            return [
+              ...accum,
+              animationObject,
+            ];
+          }
+          // Return non-animation object with accumulator.
+          return [
+            ...accum,
+            { [key]: value },
+          ];
+        },
+        [],
+      );
+    };
+  };
+}
+
+export function animationStartFactory(animate, defaultProcessor) {
+  // Upon initialization, `start` cannot animate, but is required by `react-move`
+  const METHOD = 'start';
+  return (datum, index) => {
+    return reduce(
+      defaultProcessor(datum, index),
+      (accum, value, key) => {
+        const userMethod = get(animate, [key, METHOD]);
+
+        const startValues = {
+          [key]: value,
+          ...(userMethod && userMethod(value, datum, index)),
+        };
+
+        return {
+          ...accum,
+          ...startValues,
+        };
+      },
+      {},
+    );
+  };
+}
