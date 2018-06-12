6310f63cf1721492d0f5040418598ee83c0780c4
James Vermillion <vermijw@uw.edu>
2018-06-12 10:21:21 -0700

    STP-196: Add a custom mocking function that implements support on `document.createElement` in jsdom.
    This function is based on a similar solution in geospatial-app. It written in scripts/setup-canvas-jsdom.js and used in the scripts/setup-jsdom.js file.  It simply takes the jsdom version of `document.createElement` and adds a `getContext` property if the `tagLine` given is 'canvas'. JSDOM does not supply this out of the box.

---
+++ scripts/setup-canvas-jsdom.js
@@ -0,0 +1,29 @@
+/**
+ * Amends mock support in `document.createElement` for canvas elements in js-dom
+ * @param {createElement} originalCreateElement - createElement method of jsdom api on `document`.
+ * @param {string} tagName - tagName of Element to create
+ * @return {Element}
+ */
+export function createElementMock(originalCreateElement, tagName) {
+  const initialElement = originalCreateElement(tagName);
+
+  if (tagName === 'canvas') {
+    return {
+      ...initialElement,
+      getContext: () => ({
+        font: 'font',
+        measureText: text => ({ width: text.length }),
+      }),
+    };
+  }
+
+  return initialElement;
+}
+
+/**
+ * @function createElement
+ * @description Intended to be used as the jsdom emulation of `document.createElement`, which is an
+ * [HTML document method that creates the HTML element specified by tagName.](https://developer.mozilla.org/en-US/docs/Web/API/Element)
+ * @param {string} tagName - specifies the type of element to be created
+ * @returns {Element}
+ */
